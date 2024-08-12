ed985344a834cd9753297bb7a8604deb0d1cd952
Bruno Silva <brunosilvabrait@gmail.com>
2024-08-12 19:24:23 -0300

    App and json files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { Task } from "./src/components/Task";
+import { CardNumber } from "./src/components/CardNumber";
+import { CardInOpen } from "./src/components/CardInOpen";
+import { CardDone } from "./src/components/CardDone";
+import { InputAddTask } from "./src/components/InputAddTask";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<InputAddTask />
+			<View style={{ flexDirection: "row", gap: 16 }}>
+				<CardNumber />
+				<CardInOpen />
+				<CardDone />
+			</View>
+			<Task />
+			<Task />
+			<Task />
+			<Task />
+			<Task />
+			<StatusBar style='auto' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#28385E",
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 16,
+		gap: 16
+	}
 });
