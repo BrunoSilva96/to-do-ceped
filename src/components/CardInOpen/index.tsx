70a45278706c582584753ebdf96f067bdc6c1a53
Bruno Silva <brunosilvabrait@gmail.com>
2024-08-12 19:22:48 -0300

    Open task numbers component

---
+++ src/components/CardInOpen/index.tsx
@@ -0,0 +1,10 @@
+import { Container, CardTitle, CardValue } from "./styles";
+
+export function CardInOpen() {
+	return (
+		<Container>
+			<CardTitle>Em aberto:</CardTitle>
+			<CardValue>3</CardValue>
+		</Container>
+	);
+}
