39df56c642387e5100a6ddb5815110cf0c063c4f
Bruno Silva <brunosilvabrait@gmail.com>
2024-08-12 19:23:13 -0300

    Done task numbers component

---
+++ src/components/CardDone/index.tsx
@@ -0,0 +1,10 @@
+import { Container, CardTitle, CardValue } from "./styles";
+
+export function CardDone() {
+	return (
+		<Container>
+			<CardTitle>Finalizadas:</CardTitle>
+			<CardValue>2</CardValue>
+		</Container>
+	);
+}
