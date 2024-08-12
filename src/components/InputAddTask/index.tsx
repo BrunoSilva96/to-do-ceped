94912edfd6516636379e0f11f7fd18758f912636
Bruno Silva <brunosilvabrait@gmail.com>
2024-08-12 19:23:50 -0300

    Input task component

---
+++ src/components/InputAddTask/index.tsx
@@ -0,0 +1,13 @@
+import { Feather } from "@expo/vector-icons";
+import { InputContainer, Input, InputButton } from "./styles";
+
+export function InputAddTask() {
+	return (
+		<InputContainer>
+			<Input placeholder='Digite aqui sua tarefa.' placeholderTextColor='white' keyboardType='default' />
+			<InputButton>
+				<Feather name='plus-square' size={24} color='white' />
+			</InputButton>
+		</InputContainer>
+	);
+}
